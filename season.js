e4c39493ff79450e9c6c0a709ee700251cd93c19
KurubaMohanGowd <138500065+KurubaMohanGowd@users.noreply.github.com>
2023-07-13 11:05:03 +0530

    Add files via upload

---
+++ season.js
@@ -0,0 +1,19 @@
+let sprImg=document.getElementById("seasonSmallImage")
+let mdimg=document.getElementById("seasonMediumImage")
+function spr(){
+    
+    sprImg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png"
+    mdimg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+}
+function sum(){
+    sprImg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png"
+    mdimg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png"
+}
+function aut(){
+    sprImg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png"
+    mdimg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+}
+function win(){
+    sprImg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png"
+    mdimg.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png"
+}
